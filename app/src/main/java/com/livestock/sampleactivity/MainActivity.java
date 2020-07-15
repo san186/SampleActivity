1ac1812f8f44e70eae35eb4e3af84af8e363ef65
Santhosh <sanmiphone@gmail.com>
2020-07-15 13:20:35 +0530

    Vijay Changes

--- app/src/main/java/com/livestock/sampleactivity/MainActivity.java
+++ app/src/main/java/com/livestock/sampleactivity/MainActivity.java
@@ -4,17 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Button Btn1;
     Button Btn;
+
+    EditText Edt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button Btn;
 
 
 
