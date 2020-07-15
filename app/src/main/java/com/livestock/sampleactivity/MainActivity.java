3a3aa35b7b11ac5db1c8b3833dcfaa811afb75ec
Santhosh <sanmiphone@gmail.com>
2020-07-15 12:24:48 +0530

    intial commit

---
+++ app/src/main/java/com/livestock/sampleactivity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.livestock.sampleactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button Btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
